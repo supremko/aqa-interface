279cdd3076a35192cdf7bfa0cc7d569a167501b9
Kirill Lepilov <supremko.fun@gmail.com>
2019-12-15 13:57:49 +0300

    change test

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -7,13 +7,13 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CallbackTest {
     @Test
-    void shouldSubmitRequest() throws InterruptedException {
+    void shouldSubmitRequest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
-        form.$("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        form.$("button").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
